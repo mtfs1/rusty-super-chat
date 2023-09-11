f583a8cf001671ac731ba426285ca091859d3024
mtfs1 <math.santos.2002@gmail.com>
2023-09-10 21:28:31 -0300

    feat(client): print welcome header

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    print_header();
 }
+
+fn print_header() {
+    println!("[SUPER CHAT]");
+    println!("Bem vindo ao super chat! \
+        Digite o endereco do servidor que \
+        deseja conectar-se:");
+}
+
