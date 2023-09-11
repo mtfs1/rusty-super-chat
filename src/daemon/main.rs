e0b5c9f19ad6c6be4768ee0dde7dbb10cde1fe12
mtfs1 <math.santos.2002@gmail.com>
2023-09-11 00:24:57 -0300

    feat(daemon): register user to a room

--- src/daemon/main.rs
+++ src/daemon/main.rs
@@ -1,17 +1,56 @@
+use std::collections::HashMap;
 use std::io::Read;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::sync::{Mutex, Arc};
 use std::thread;
 
 
 fn main() -> std::io::Result<()> {
     let socket = TcpListener::bind("0.0.0.0:1234")?;
 
+    let rooms = Arc::new(
+        Mutex::new(HashMap::<String, Vec<TcpStream>>::new())
+    );
+
     for conn in socket.incoming() {
         let mut conn = conn?;
+        let rooms = rooms.clone();
+
         thread::spawn(move || {
             let mut buff = String::new();
             conn.read_to_string(&mut buff).unwrap();
-            println!("{buff}");
+
+            let mut iter = buff.split("&");
+            let name = match iter.next() {
+                Some(val) => val.to_string(),
+                None => {
+                    println!("[INFO] Connection failed, no name \
+                        informed for user");
+                    return;
+                }
+            };
+            let room = match iter.next() {
+                Some(val) => val.to_string(),
+                None => {
+                    println!("[INFO] Connection failed, no room \
+                        informed for user");
+                    return;
+                }
+            };
+
+            {
+                let mut rooms = rooms.lock().unwrap();
+                let room = {
+                    if let None = rooms.get(&room) {
+                        rooms.insert(room.clone(), Vec::new());
+                    }
+                    rooms.get_mut(&room).unwrap()
+                };
+                room.push(conn.try_clone().unwrap());
+            }
+
+            println!("[INFO] User {name} succesfully entered \
+                room {room}");
         });
     }
 
