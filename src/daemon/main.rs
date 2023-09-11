e31006aa0545324a750928e693987eb9e44a73c9
mtfs1 <math.santos.2002@gmail.com>
2023-09-10 21:18:28 -0300

    feat(daemon): basic TCP listener

--- src/daemon/main.rs
+++ src/daemon/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+use std::net::TcpListener;
+use std::thread;
+
+
+fn main() -> std::io::Result<()> {
+    let socket = TcpListener::bind("0.0.0.0:1234")?;
+
+    for conn in socket.incoming() {
+        let mut conn = conn?;
+        thread::spawn(move || {
+            let mut buff = String::new();
+            conn.read_to_string(&mut buff).unwrap();
+            println!("{buff}");
+        });
+    }
+
+    Ok(())
 }
+
